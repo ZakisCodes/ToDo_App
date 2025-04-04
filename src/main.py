c9cc03e14a1e3d1108ec1a9500ff22b5bd4c03c5
Zaki <zakinabdul.tech@gmail.com>
2025-04-04 08:38:10 +0530

    Connected html page with fastapi

--- src/main.py
+++ src/main.py
@@ -1,2 +1,25 @@
-for i in range(10):
-    print(i)
+from tempfile import template
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+import uvicorn
+
+app = FastAPI()
+
+template = Jinja2Templates(directory="templates")
+
+@app.get('/')
+def index(req: Request):
+    return template.TemplateResponse(
+        name="index.html",
+        context={"request": req}
+    )
+
+@app.get("/test")
+def login(req: Request):
+    return template.TemplateResponse(
+        name="login.html",
+        context={"request": req}
+    )
+
+if __name__ == "__main__":
+    uvicorn.run("main.app")
